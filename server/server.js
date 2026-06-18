9c0ea8a4a28d8269de5c5b8f093f711c16540a84
Aditya Kumar <adityatechspace@gmail.com>
2026-06-18 12:11:46 +0530

    Error finding

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ dotenv.config();
 
 const { default: app } = await import("./app.js");
 
+console.log("About to start server...");
+
+
 const PORT = process.env.PORT || 5000;
 
-console.log("About to start server...");
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
