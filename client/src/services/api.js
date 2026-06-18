11fd831deda11c9ac814bcdb98e89803ef2a570d
Aditya Kumar <adityatechspace@gmail.com>
2026-06-18 19:44:57 +0530

    Base URL updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API =
   axios.create({
     baseURL:
-      "http://localhost:5000/api",
+      "https://portfolio-zz8d.onrender.com/api",
   });
 
 export const askAssistant =
