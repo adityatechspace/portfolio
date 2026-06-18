eb16b1694dbbb2bc99a6edefdf3f604bcaafae33
Aditya Kumar <adityatechspace@gmail.com>
2026-06-18 12:09:25 +0530

    Error finding

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { default: app } = await import("./app.js");
 
 const PORT = process.env.PORT || 5000;
 
+console.log("About to start server...");
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
